cddf42fbc49bd838cb96a99b82a269b7b42c4d21
Andreas Gohr <andi@splitbrain.org>
2017-03-20 21:25:13 +0100

    properly detect the runtime environment

--- background.js
+++ background.js
@@ -1,30 +1,32 @@
+var rt = (typeof browser !== 'undefined') ? browser : chrome; 
+
+
 /** store the edit urls for the current tabs */
 editlinks = {};
 
 /** receive messages from the content script */
-var rt = browser ? browser.runtime : chrome.extension;
-rt.onMessage.addListener(function (request, sender) {
+(rt.runtime || rt.extension).onMessage.addListener(function (request, sender) {
     // store the link
     editlinks[sender.tab.id] = request.href;
 
     // Enable the page action icon.
-    (browser || chrome).pageAction.setTitle(
+    rt.pageAction.setTitle(
         {
             tabId: sender.tab.id,
             title: request.title
         }
     );
-    (browser || chrome).pageAction.show(sender.tab.id);
+    rt.pageAction.show(sender.tab.id);
 });
 
 /** remove stored urls again, when the tab is closed */
-(browser || chrome).tabs.onRemoved.addListener(function (tabId) {
+rt.tabs.onRemoved.addListener(function (tabId) {
     editlinks[tabId] = null;
 });
 
 /** handle clicks on the page icon */
-(browser || chrome).pageAction.onClicked.addListener(function (tab) {
-    (browser || chrome).tabs.update(tab.id, {
+rt.pageAction.onClicked.addListener(function (tab) {
+    rt.tabs.update(tab.id, {
         url: editlinks[tab.id]
     });
 });
--- content.js
+++ content.js
@@ -1,4 +1,6 @@
 (function () {
+    var rt = (typeof browser !== 'undefined') ? browser : chrome;
+
     // find the ueb markup
     var element = document.querySelector(
         'link[rel="edit"], ' +
@@ -10,7 +12,7 @@
     if (element && element.href) {
         var title = 'Edit this page';
         if (element.title) title = element.title;
-        var rt = browser ? browser.runtime : chrome.extension;
-        rt.sendMessage({title: title, href: element.href});
+        (rt.runtime || rt.extension).sendMessage({title: title, href: element.href});
     }
+
 })();
